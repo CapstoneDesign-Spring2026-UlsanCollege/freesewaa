0ae86e5df1c276b27f12ae1372050c1a836ddd8d
Swarnim Jung Karki <swarnimkarki60@gmail.com>
2026-04-28 09:33:53 +0900

    reorganize: Clean folder structure - html/, css/, js/, server/, docs-midterm/

--- backend/src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/notificationController');
-const requireAuth = require('../middleware/auth');
-
-const router = express.Router();
-router.get('/', requireAuth, controller.getNotifications);
-router.patch('/read', requireAuth, controller.markRead);
-
-module.exports = router;
--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FREESEWAA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA53_RfmZ2OeO3AM7X4ZqdhIwzznJsgXWs",
-  authDomain: "free-sewaa.firebaseapp.com",
-  projectId: "free-sewaa",
-  storageBucket: "free-sewaa.firebasestorage.app",
-  messagingSenderId: "46295068576",
-  appId: "1:46295068576:web:19c3e46cba6ba6c4893ec5"
-};
